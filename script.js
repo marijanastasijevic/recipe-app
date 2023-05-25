73bd94ebe0a57dfdc8fd92ef29a3a93fee27c1f5
marijanastasijevic <nastasijevicmarija27@gmail.com>
2023-05-25 15:21:58 +0200

    Add addMeal function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const meals = document.getElementById('meals')
+
 async function getRandomMeal(){
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
 
@@ -5,6 +7,8 @@ async function getRandomMeal(){
     const randomMeal = respData.meals[0]
 
     console.log(randomMeal);
+
+    addMeal(randomMeal, true)
     
 }
 
@@ -14,6 +18,31 @@ async function getMealById(id){
 
 async function getMealsBySearch(name){
    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+name)
+}
+
+//postavljanje vrednosti parametru funkcije je difoltna vrenost
+function addMeal(mealData, random = false){
+    const meal = document.createElement("div");
+    meal.classList.add('meal');
+
+    meal.innerHTML = `
+        <div class="meal-header">
+            ${random ? ` <span class="random">Random Recipe</span>` : ''}
+           
+            <img 
+                src="${mealData.strMealThumb}" 
+                alt="${mealData.strMeal}"
+            />
+        </div>
+
+        <div class="meal-body">
+            <h2>${mealData.strMeal}</h2>
+            <button class="fav-btn">
+                <i class="fa-regular fa-heart"></i></i>
+            </button>
+        </div>   
+    `
+    meals.appendChild(meal)
 }
 
 getRandomMeal()
