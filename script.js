77f6ab362f3c574b4d91dec1d3995b2b068600ab
Marija Daskalovic <77746232+marijanastasijevic@users.noreply.github.com>
2023-06-12 23:31:33 +0200

    Add function fetchFavMeals and addMealtoFav (#8)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const meals = document.getElementById('meals');
 const favoriteContainer = document.getElementById("fav-meals");
+getRandomMeal();
+fetchFavMeals();
 
 async function getRandomMeal(){
     const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
@@ -12,6 +14,8 @@ async function getRandomMeal(){
     addMeal(randomMeal, true)
     
 }
+
+
 
 async function getMealById(id){
     const resp = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+id);
@@ -93,5 +97,42 @@ function getMealsfromLS(){
 
     return mealIds === null ? [] : mealIds;
 }
+
+async function fetchFavMeals(){
+    const mealIds = getMealsfromLS();
+
+    for(let i = 0; i < mealIds.length; i++){
+        const mealId = mealIds[i];
+        const meal = await getMealById(mealId)
+
+        addMealtoFav(meal);
+
+        console.log(meals);
+
+        // add them to the screen
+
+    }
+
+
+}
+
+function addMealtoFav(mealData){
+    const favMeal = document.createElement("li");
+   
+
+    favMeal.innerHTML = `
+        <li>
+            <img 
+                src="${mealData.strMealThumb}" 
+                alt="${mealData.strMeal}"
+            />
+            <span>${mealData.strMeal}</span>
+        </li>
+    `
+
+    
+    favoriteContainer.appendChild(favMeal);
+}
+
+
  
-getRandomMeal()
